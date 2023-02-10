517a9b0707d3c3f12607bcf86da7dadadd4880f8
raymbox <quadrayxl@gmail.com>
2023-02-10 09:32:36 -0500

    MOD07 EX00 add the main ask by the subject

--- MOD07/ex00/main.cpp
+++ MOD07/ex00/main.cpp
@@ -1,12 +1,14 @@
 #include "whatever.hpp"
 
-int main()
+void personnal_test()
 {
 	int		i[2][2] = {{2 , 1}, {1 , 2}};
 	float	f[2][2] = {{2.1f , 2.6f}, {5.4f , 2.2f}};
 	double	d[2][2] = {{2.1 , 2.6}, {5.4 , 2.2}};
 	bool	b[2][2] = {{true, false}, {false, true}};
 	char	c[2][2] = {{'a', 'z'}, {'z', 'a'}};
+
+	std::cout << "\n----PERSONNAL TESTS----" << std::endl;
 
 	for (int x = 0; x < 2; x++)
 	{
@@ -17,7 +19,27 @@ int main()
 		tests(c[x]);
 	}
 
-	return (0);
 }
 
 
+int main( void )
+{
+	std::cout << "-----MANDATORY TESTS-----" << std::endl;
+
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
+	personnal_test();
+	
+	return 0;
+}
